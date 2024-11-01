75a0414be89a4037e5095097e376c302c0524805
thaissan <thais.cavadas@hotmail.com>
2024-11-01 12:40:28 -0300

    sidebar adicionada

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,10 +3,30 @@ import { PiCrownSimpleBold } from "react-icons/pi";
 import { GrInbox } from "react-icons/gr";
 import { FaRegCommentAlt } from "react-icons/fa";
 import { CiBatteryCharging } from "react-icons/ci";
-import {Container, Header, Navbar, Search, IconsContainer, UserIcon} from './style'
+import {Container, Header, Navbar, Search, IconsContainer, UserIcon,
+  Sidebar, SidebarDivs, ChannelList, ChannelItem, RedCircle
+} from './style'
+import { TbArrowBarLeft } from "react-icons/tb";
+import { LuArrowDownUp } from "react-icons/lu";
+
+
 
 
 export const TwitchHome = () => {
+
+  const followedChannels = [
+    { id: 1, name: 'casimito', img: 'https://static-cdn.jtvnw.net/jtv_user_pictures/32805a78-d927-48bd-8089-bf5efed53ea4-profile_image-70x70.png', status: 'Offline' },
+    { id: 2, name: 'Gaules', img: 'https://static-cdn.jtvnw.net/jtv_user_pictures/ea0fe422-84bd-4aee-9d10-fd4b0b3a7054-profile_image-70x70.png', status: 'Offline' }
+  ];
+
+  const recommendedChannels = [
+    { id: 1, name: 'Ninja', img: 'https://static-cdn.jtvnw.net/jtv_user_pictures/90d40495-f467-4911-9035-72d8d10a49c5-profile_image-70x70.png', viewers: '10mil' },
+    { id: 2, name: 'tcK10', img: 'https://static-cdn.jtvnw.net/jtv_user_pictures/4e24a619-4154-4d58-b8af-fc4dad46d521-profile_image-70x70.png',viewers: '8mil' },
+    { id: 3, name: 'Pokimane', img: 'https://static-cdn.jtvnw.net/jtv_user_pictures/6cd4de40-1a83-46c7-aea5-3bd73f90e7e4-profile_image-70x70.png', viewers: '6mil' },
+    { id: 4, name: 'Shroud', img: 'https://static-cdn.jtvnw.net/jtv_user_pictures/7ed5e0c6-0191-4eef-8328-4af6e4ea5318-profile_image-70x70.png', viewers: '5mil' },
+    { id: 5, name: 'YoDa', img: 'https://static-cdn.jtvnw.net/jtv_user_pictures/e5e89aba-723b-4ba1-852c-d8fba19b1da1-profile_image-70x70.png', viewers: '4mil' },
+    { id: 6, name: 'manodeyvin', img: 'https://static-cdn.jtvnw.net/jtv_user_pictures/2a0593d4-48f7-4ff2-bf39-c77888227c1b-profile_image-70x70.png', viewers: '4mil' },
+  ];
 
     return(
     <Container>
@@ -41,6 +61,52 @@ export const TwitchHome = () => {
           </UserIcon>
         </IconsContainer>
       </Header>
+
+      <div style={{ display: 'flex' }}>
+        <Sidebar>
+          <SidebarDivs>
+            <h4>Para você</h4>
+            <TbArrowBarLeft />
+          </SidebarDivs>
+
+          <SidebarDivs style={{paddingTop: 10}}>
+            <span id="titleFollowed">CANAIS SEGUIDOS</span>
+            <LuArrowDownUp />
+          </SidebarDivs>
+          <ChannelList>
+            {followedChannels.map(channel => (
+              <ChannelItem key={channel.id}>
+                <img src={channel.img} alt={channel.name} />
+                <SidebarDivs>
+                  <p>{channel.name}</p>
+                  <small>{channel.status}</small>
+                </SidebarDivs>
+              </ChannelItem>
+            ))}
+          </ChannelList>
+
+          <SidebarDivs style={{paddingTop: 10}}>
+            <span id="titleRecommended">CANAIS RECOMENDADOS</span>
+            <LuArrowDownUp />
+          </SidebarDivs>
+          <ChannelList>
+            {recommendedChannels.map(channel => (
+              <ChannelItem key={channel.id}>
+                <img src={channel.img} alt={channel.name} />
+                <SidebarDivs>
+                  <p>{channel.name}</p>
+                  <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', gap: 5}}>
+                  <RedCircle> </RedCircle>
+                  <small>{channel.viewers}</small>  
+                  </div>
+                </SidebarDivs>
+              </ChannelItem>
+            ))}
+            <a href="#">Mostrar mais</a>
+          </ChannelList>
+        </Sidebar>
+
+      </div>
 
     </Container>
     )
--- src/pages/Home/style.jsx
+++ src/pages/Home/style.jsx
@@ -2,13 +2,14 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   max-width: 100%;
-  padding: 0 20px;
+  padding: 0px;
 `;
 
 export const Header = styled.header`
   display: flex;
   justify-content: space-between;
-  padding: 10px 0;
+  padding: 4px 5px;
+  background-color: white;
   border-bottom: 1px solid #e5e5e5;
   flex-wrap: wrap;
 
@@ -93,3 +94,65 @@ export const UserIcon = styled.div`
   }
 
 `;
+
+//main
+export const Sidebar = styled.aside`
+  width: 230px;
+  height: 100vh;
+  background-color: #EFEFF1;
+  border-right: 1px solid #e5e5e5;
+  padding: 9px;
+`;
+
+export const SidebarDivs = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+
+  #titleFollowed{
+    font-weight: 600;
+    font-size: smaller;
+    color: #8e8e8f;
+  }
+  
+  #titleRecommended{
+    font-weight: 700;
+    font-size: smaller;
+    color: #000000;
+  }
+`;
+
+export const ChannelList = styled.div`
+  margin-top: 5px;
+
+  a{
+    text-decoration: none;
+    color: #7F5EEA;
+    font-size: smaller;
+  }
+`;
+
+export const ChannelItem = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  padding: 8px 0;
+
+  p{
+    font-weight: 600;
+  }
+  
+  img {
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+  }
+`;
+
+export const RedCircle = styled.div`
+  width: 7px;         
+  height: 7px;          
+  background-color: red; 
+  border-radius: 50%;
+`;   
